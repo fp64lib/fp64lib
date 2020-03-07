773c4a64a35b80e4a08b24b28beeb4a2586496a1
fp64lib <mail@fp64lib.org>
2020-03-07 17:41:38 +0100

    Added more possibilities to class Double

--- examples/Double/Double.ino
+++ examples/Double/Double.ino
@@ -11,28 +11,55 @@ class Double {
 		Double()				{ x = 0ULL; }
 		Double( double f )		{ x = fp64_sd( f ); }
 		Double( float f )		{ x = fp64_sd( f ); }
+		Double( int16_t n )		{ x = fp64_int16_to_float64( n ); }
+		Double( uint16_t n )	{ x = fp64_uint16_to_float64( n ); }
 		Double( const Double& d ) { x = d.x; }
 		const char* toString() { 
 			return fp64_to_string( x, 17, 15);
 			}
+		Double operator+=( const Double& y ) {
+			this->x = fp64_add( this->x, y.x );
+			return( *this );
+			}
 		Double operator+( const Double& y ) {
 			Double res;
-			res.x = fp64_add( x, y.x );
+			res.x = fp64_add( this->x, y.x );
 			return( res );
+			}
+		Double operator-=( const Double& y ) {
+			this->x = fp64_sub( this->x, y.x );
+			return( *this );
 			}
 		Double operator-( const Double& y ) {
 			Double res;
-			res.x = fp64_sub( x, y.x );
+			res.x = fp64_sub( this->x, y.x );
 			return( res );
+			}
+		Double operator*=( const Double& y ) {
+			this->x = fp64_mul( this->x, y.x );
+			return( *this );
 			}
 		Double operator*( const Double& y ) {
 			Double res;
-			res.x = fp64_mul( x, y.x );
+			res.x = fp64_mul( this->x, y.x );
 			return( res );
+			}
+		Double operator/=( const Double& y ) {
+			this->x = fp64_div( this->x, y.x );
+			return( *this );
 			}
 		Double operator/( const Double& y ) {
 			Double res;
-			res.x = fp64_div( x, y.x );
+			res.x = fp64_div( this->x, y.x );
+			return( res );
+			}
+		Double operator%=( const Double& y ) {
+			this->x = fp64_fmod( this->x, y.x );
+			return( *this );
+			}
+		Double operator%( const Double &y ) {
+			Double res;
+			res.x = fp64_fmod( this->x, y.x );
 			return( res );
 			}
    private:
@@ -40,13 +67,23 @@ class Double {
 };
 
 #define MAX_COUNT   1000    // # of iterations before printing another approximation
-Double x = Double( 1.0 );
-Double n = Double( 3.0 );
-Double delta = Double( 2.0 );
+Double x = 1.0;
+Double n = 3.0;
+Double delta = 2;
 bool add = false;
 
 void setup() {
 	Serial.begin(57600);
+	
+	Double a = 1;
+	Double b;
+	Serial.println( "Test for implicit type casting: b = a + 2" );
+	Serial.print("a:"); Serial.println( a.toString() );
+	b = a + 2;
+	Serial.print("b:"); Serial.println( b.toString() );
+	Serial.println( "Test for implicit type casting: a += 3" );
+	a += 3;
+	Serial.print("a:"); Serial.println( a.toString() );
 }
 
 void loop() {
@@ -54,20 +91,20 @@ void loop() {
 
 	while(1) {
 		if( add )
-			x = x + Double(1.0)/n;
+			x += Double(1.0)/n;
 		else
-			x = x - Double(1.0)/n;
+			x -= Double(1.0)/n;
 		add = !add;
 		if( ++cnt == MAX_COUNT ) {
 			Serial.print( n.toString() );
 			Serial.print(": ");
 			Serial.print( x.toString() );
 			Serial.print("\t");
-			Double pi = x * Double(4.0);
+			Double pi = x * 4;
 			Serial.println( pi.toString() );
 			cnt = 0;
 		}
-		n = n + Double(2.0);
+		n += 2;
 	}
 }
 	
